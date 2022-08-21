e6adc7ef6cf43db6998b4eb16a121af94195e748
Athos Schrapett <athosschrapett@outlook.com>
2022-08-21 12:42:01 -0300

    Criação de classes ferindo os principios de OCP

---
+++ SOLID/Solid/Ocp/Program.cs
@@ -0,0 +1,16 @@
+﻿using Ocp;
+
+TypeVehicle type = TypeVehicle.Motorcycle;
+
+if (type == TypeVehicle.Car)
+{
+    Vehicle vehicle = new("Azul", 2022, 2.0, 5, 4);
+    vehicle.Car();
+}
+else
+{
+    Vehicle vehicle = new("Branco", 2023, 250, 1, 0);
+    vehicle.Motorcycle();
+}
+
+Console.ReadLine();
---
+++ SOLID/Solid/Ocp/Vehicle.cs
@@ -0,0 +1,37 @@
+﻿namespace Ocp
+{
+    public class Vehicle
+    {
+        public string Color { get; set; }
+        public int Year { get; set; }
+        public double Engine { get; set; }
+        public int Seats { get; set; }
+        public int Doors { get; set; }
+
+        public Vehicle(string color, int year, double engine, int seats, int doors)
+        {
+            Color = color;
+            Year = year;
+            Engine = engine;
+            Seats = seats;
+            Doors = doors;
+        }
+
+        public void Car()
+        {
+            Console.WriteLine($"Criando um Carro {Color}, {Year}, {Engine}, {Doors} portas e {Seats} assentos.");
+            StartVehicle();
+        }
+
+        public void Motorcycle()
+        {
+            Console.WriteLine($"Criando um Carro {Color}, {Year}, {Engine} cilindradas");
+            StartVehicle();
+        }
+
+        public void StartVehicle()
+        {
+            Console.WriteLine("Ligando os motores!");
+        }
+    }
+}
