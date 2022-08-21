bca1281b3362ab2e6b8061f0bd764bdef4b85125
Athos Schrapett <athosschrapett@outlook.com>
2022-08-21 12:02:44 -0300

    Criacao de metodos incorretamente na classe principal

--- SOLID/Solid/Srp/Program.cs
+++ SOLID/Solid/Srp/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿static void CreateClient()
+{
+
+}
+
+static void ReadClient()
+{
+
+}
+
+static void UpdateClient()
+{
+
+}
+
+static void DeleteClient()
+{
+
+}
+
+static void NotifyClient()
+{
+
+}
