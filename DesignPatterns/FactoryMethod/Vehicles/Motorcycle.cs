f7d2174c0fa57a6737ce3ba345df10ba6cd4642f
Athos Schrapett <athosschrapett@outlook.com>
2022-08-28 09:36:17 -0300

    Inclusão projeto Design Patterns

---
+++ DesignPatterns/FactoryMethod/Vehicles/Motorcycle.cs
@@ -0,0 +1,15 @@
+﻿namespace FactoryMethod.Vehicles
+{
+    public class Motorcycle : IVehicle
+    {
+        public void GetGargo()
+        {
+            Console.WriteLine("Pegamos a encomenda!");
+        }
+
+        public void StartRoute()
+        {
+            Console.WriteLine("Iniciando a entrega.");
+        }
+    }
+}
