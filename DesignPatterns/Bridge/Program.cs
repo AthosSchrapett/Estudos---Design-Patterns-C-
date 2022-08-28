0eb9b32f935e08bee92e373076db0deb0fac3883
Athos Schrapett <athosschrapett@outlook.com>
2022-08-28 18:31:03 -0300

    Inclusão Bridge

---
+++ DesignPatterns/Bridge/Program.cs
@@ -0,0 +1,33 @@
+﻿using Bridge.Platforms;
+using Bridge.Transmissions;
+
+static void StartLive(IPlatform platform)
+{
+    Console.WriteLine("Aguarde...");
+
+    Live live = new Live(platform);
+
+    live.Broadcasting();
+    live.Result();
+}
+
+static void StartLiveAdvanced(IPlatform platform)
+{
+    Console.WriteLine("Aguarde...");
+
+    AdvancedLive live = new AdvancedLive(platform);
+
+    live.Broadcasting();
+    live.Subtitle();
+    live.Comments();
+    live.Result();
+}
+
+Console.WriteLine("------------------------------------------");
+StartLiveAdvanced(new Youtube());
+Console.WriteLine("------------------------------------------");
+StartLiveAdvanced(new Facebook());
+Console.WriteLine("------------------------------------------");
+StartLiveAdvanced(new TwitchTV());
+Console.WriteLine("------------------------------------------");
+Console.ReadLine();
---
+++ DesignPatterns/Bridge/Transmissions/Live.cs
@@ -0,0 +1,29 @@
+﻿using Bridge.Platforms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bridge.Transmissions
+{
+    public class Live : ITransmission
+    {
+        protected IPlatform platform;
+        
+        public Live(IPlatform platform)
+        {
+           this.platform = platform;
+        }
+
+        public void Broadcasting()
+        {
+            Console.WriteLine($"Iniciando a transmissão na plataforma {platform}");
+        }
+
+        public void Result()
+        {
+            Console.WriteLine("**** ON AIR ****");
+        }
+    }
+}
